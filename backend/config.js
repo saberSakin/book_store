b61e873c50ae17c073ff6152776bf703c9774594
Mohammad Shakin <md.shakin18@gmail.com>
2024-08-13 20:50:37 +0600

    database added

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+// config.js
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@books-store-mern.k6ypy.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
 import express from "express";
+import { PORT, mongoDBURL } from "./config.js";  // Ensure correct import
+import mongoose from "mongoose";
 
-const app= express();
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('welcome to mern stack');
+});
+
+mongoose
+   .connect(mongoDBURL)  // Use the correctly imported variable
+   .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+   })
+   .catch((error) => {
+    console.log(error);
+   });
