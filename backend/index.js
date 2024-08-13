f4c5d3b807702b933e16f32639cd22ff858b359f
Mohammad Shakin <md.shakin18@gmail.com>
2024-08-13 22:45:46 +0600

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,47 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";  // Ensure correct import
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
+
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('welcome to mern stack');
 });
+
+
+app.post('/books', async (request, response)=>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear 
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, authot, publishYear'
+            })
+        }
+        const newBook ={
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        }
+        const book=await Book.create(newBook);
+
+        return response.status(201).send(book);
+
+
+    } catch (error){
+        console.log(error.message)
+        response.status(500).send({message: error.message})
+    }
+});
+
+
 
 mongoose
    .connect(mongoDBURL)  // Use the correctly imported variable
